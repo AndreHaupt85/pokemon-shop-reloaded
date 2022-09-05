55badcd35cfa0be6d14fb840caf706b9b32a6e0b
Andre Haupt <andrehaupt@me.com>
2022-09-05 11:55:31 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/home"
+import { Shop } from "./pages/shop"
+import { Cart } from "./pages/cart"
+
+
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
     </div>
   );
 }
